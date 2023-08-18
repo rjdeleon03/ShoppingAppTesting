d3f5ff95a40ee97850f2102d14fb4d0dcc83d4f9
Ricardo De Leon <rpdeleon.dev@gmail.com>
2023-08-18 13:55:21 +0800

    create repository and interface

---
+++ app/src/main/java/com/androiddevs/shoppinglisttestingyt/repositories/ShoppingRepository.kt
@@ -0,0 +1,19 @@
+package com.androiddevs.shoppinglisttestingyt.repositories
+
+import androidx.lifecycle.LiveData
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItem
+import com.androiddevs.shoppinglisttestingyt.data.remote.responses.ImageResponse
+import com.androiddevs.shoppinglisttestingyt.other.Resource
+
+interface ShoppingRepository {
+
+    suspend fun insertShoppingItem(shoppingItem: ShoppingItem)
+
+    suspend fun deleteShoppingItem(shoppingItem: ShoppingItem)
+
+    fun observeAllShoppingItems(): LiveData<List<ShoppingItem>>
+
+    fun observeTotalPrice(): LiveData<Float>
+
+    suspend fun searchForImage(imageQuery: String): Resource<ImageResponse>
+}
