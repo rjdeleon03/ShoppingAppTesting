063a59a6fb32015fc275e728d55d18de897c7e42
Ricardo De Leon <rpdeleon.dev@gmail.com>
2023-08-18 13:05:32 +0800

    add hilt module

---
+++ app/src/main/java/com/androiddevs/shoppinglisttestingyt/di/AppModule.kt
@@ -0,0 +1,55 @@
+package com.androiddevs.shoppinglisttestingyt.di
+
+import android.content.Context
+import androidx.room.Room
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingDao
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItemDatabase
+import com.androiddevs.shoppinglisttestingyt.data.remote.PixabayAPI
+import com.androiddevs.shoppinglisttestingyt.other.Constants.BASE_URL
+import com.androiddevs.shoppinglisttestingyt.other.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+// Changed to SingletonComponent for Hilt to be
+// more inclusive of non-Android projects
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideShoppingItemDatabase(
+        @ApplicationContext context: Context
+    ): ShoppingItemDatabase {
+        return Room.databaseBuilder(
+            context,
+            ShoppingItemDatabase::class.java,
+            DATABASE_NAME
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideShoppingDao(
+        database: ShoppingItemDatabase
+    ): ShoppingDao {
+        return database.shoppingDao()
+    }
+
+    @Singleton
+    @Provides
+    fun providePixabayAPI(): PixabayAPI {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create()
+    }
+}
