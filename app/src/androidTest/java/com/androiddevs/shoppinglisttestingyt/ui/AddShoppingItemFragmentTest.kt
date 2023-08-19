88cc267ac36666d9c6b4335035729bfc2ed8ee97
Ricardo De Leon <rpdeleon.dev@gmail.com>
2023-08-19 14:12:01 +0800

    test add shopping fragment navigation

--- app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/ui/AddShoppingItemFragmentTest.kt
+++ app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/ui/AddShoppingItemFragmentTest.kt
@@ -2,25 +2,23 @@ package com.androiddevs.shoppinglisttestingyt.ui
 
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.filters.MediumTest
-import com.androiddevs.shoppinglisttestingyt.R
 import com.androiddevs.shoppinglisttestingyt.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 
-@MediumTest // Since this is an Integration Test
+@MediumTest
 @HiltAndroidTest
 @ExperimentalCoroutinesApi
-class ShoppingFragmentTest {
+class AddShoppingItemFragmentTest {
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
@@ -31,20 +29,14 @@ class ShoppingFragmentTest {
     }
 
     @Test
-    fun clickAddShoppingItemButton_navigateToAddShoppingItemFragment() {
+    fun pressBackButton_popBackStack() {
         val navController = mock(NavController::class.java)
-        launchFragmentInHiltContainer<ShoppingFragment> {
+        launchFragmentInHiltContainer<AddShoppingItemFragment> {
             Navigation.setViewNavController(requireView(), navController)
         }
 
-        // Find the button, and perform click
-        onView(withId(R.id.fabAddShoppingItem)).perform(click())
+        pressBack()
 
-        // Verify that the call was done correctly
-        verify(navController).navigate(
-            ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
-        )
-
-
+        verify(navController).popBackStack()
     }
 }
